d7ad0b6fbbb89477df4b47a4e466bb086da7e95c
4reha <ael-hadd@student.1337.ma>
2023-02-04 17:13:09 +0100

    prisma service

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,16 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-  constructor() {
+  constructor(config: ConfigService) {
     super({
       datasources: {
         db: {
-          url: process.env.DATABASE_URL,
+          url: config.get('DATABASE_URL'),
         },
       },
     });
+    // console.log('PrismaService: ', config.get('DATABASE_URL'));
+    // console.log(process.env.DATABASE_URL);
   }
   async resetDb() {
     await this.bookmark.deleteMany({});
