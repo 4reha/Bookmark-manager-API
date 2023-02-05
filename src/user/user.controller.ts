3b0c0ec2ebed31693aa510afc1992f6b49edbbec
Abderrahman El haddad asofi <ael-hadd@e1r10p12.1337.ma>
2023-02-05 19:26:41 +0100

    Completing user module

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,29 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Patch,
+  UseGuards,
+} from '@nestjs/common';
 import { User } from '@prisma/client';
 import { GetUser } from './../auth/decorator';
+import { EditUserDto } from './dto/edit-user.dto';
 import { JwtGuard } from './../auth/guard';
+import { UserService } from './user.service';
 
 @UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
+  constructor(private userService: UserService) {}
   @Get('me')
   getMe(@GetUser() user: User, @GetUser('username') username: string) {
     console.log(username);
     return user;
+  }
+
+  @Patch()
+  editUser(@GetUser('id') userId: number, @Body() dto: EditUserDto) {
+    return this.userService.editUser(userId, dto);
   }
 }
